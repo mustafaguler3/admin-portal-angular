b99c1779885aa0fd073d917fbd26c74e9b365c05
mustafaguler3 <mustafa.glr@hotmail.com>
2023-07-11 12:19:33 +0300

    added navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+
+  getDisplay(){
+
+  }
+
+  logout(){
+    
+  }
+
+}
